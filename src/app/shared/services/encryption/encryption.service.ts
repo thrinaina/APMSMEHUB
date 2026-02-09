5d10208c34b131cbad28960a2833062b3a8f18c5
sekharthrinaina <sekhar@thrinaina.com>
2026-02-09 17:18:49 +0530

    Initial Changes, Encryption Keys Removed

--- src/app/shared/services/encryption/encryption.service.ts
+++ src/app/shared/services/encryption/encryption.service.ts
@@ -9,34 +9,34 @@ export class EncryptionService {
 
   constructor() { }
 
-  private secretKey = environment.ENCRYPTION_SECRET_KEY; // Use a strong key
-  private iv = environment.IV_KEY; // Should be 16 bytes long
+  // private secretKey = environment.ENCRYPTION_SECRET_KEY; // Use a strong key
+  // private iv = environment.IV_KEY; // Should be 16 bytes long
 
-  // Encrypt function
-  encrypt(data: any): string {
-    const key = CryptoJS.enc.Utf8.parse(this.secretKey);
-    const iv = CryptoJS.enc.Utf8.parse(this.iv);
+  // // Encrypt function
+  // encrypt(data: any): string {
+  //   const key = CryptoJS.enc.Utf8.parse(this.secretKey);
+  //   const iv = CryptoJS.enc.Utf8.parse(this.iv);
 
-    const encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), key, {
-      iv: iv,
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7,
-    });
+  //   const encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), key, {
+  //     iv: iv,
+  //     mode: CryptoJS.mode.CBC,
+  //     padding: CryptoJS.pad.Pkcs7,
+  //   });
 
-    return encrypted.toString();
-  }
+  //   return encrypted.toString();
+  // }
 
-  // Decrypt function
-  decrypt(encryptedData: string): any {
-    const key = CryptoJS.enc.Utf8.parse(this.secretKey);
-    const iv = CryptoJS.enc.Utf8.parse(this.iv);
+  // // Decrypt function
+  // decrypt(encryptedData: string): any {
+  //   const key = CryptoJS.enc.Utf8.parse(this.secretKey);
+  //   const iv = CryptoJS.enc.Utf8.parse(this.iv);
 
-    const decrypted = CryptoJS.AES.decrypt(encryptedData, key, {
-      iv: iv,
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7,
-    });
+  //   const decrypted = CryptoJS.AES.decrypt(encryptedData, key, {
+  //     iv: iv,
+  //     mode: CryptoJS.mode.CBC,
+  //     padding: CryptoJS.pad.Pkcs7,
+  //   });
 
-    return JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
-  }
+  //   return JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
+  // }
 }
